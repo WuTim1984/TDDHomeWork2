643e655fd574343727edea51f1249c697168c606
wuchienting@gmail.com <wuchienting@gmail.com>
2016-01-21 10:39:43 +0800

    Case1: 綠燈 Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元

--- TDDHOMEWORD2/TDDHW2.cs
+++ TDDHOMEWORD2/TDDHW2.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace TDDHOMEWORD2
 {
     public class TDDHW2
     {
-        public int Accounting(List<Book> booklist,int price)
+        public int Accounting(List<Book> booklist, int price)
         {
             int sum = 0;
 
-            sum = booklist.Sum(x=>x.Quantity);
+            sum = booklist.Sum(x => x.Quantity);
 
-            return sum*price;
+            return sum * price;
         }
     }
-
 
     public class Book
     {
         public int Id { get; set; }
 
         public int Quantity { get; set; }
-
     }
 }
--- TDDHOMEWORD2Tests/TDDHW2Tests.cs
+++ TDDHOMEWORD2Tests/TDDHW2Tests.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TDDHOMEWORD2;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace TDDHOMEWORD2.Tests
 {
     [TestClass()]
@@ -13,23 +9,23 @@ namespace TDDHOMEWORD2.Tests
         [TestMethod()]
         public void AccountingTest_id_1_quantity_1_shouldbe_100()
         {
-            //arrange   
-            List<Book> book = new List<Book>() { 
+            //arrange
+            List<Book> book = new List<Book>() {
             new Book{Id=1,Quantity = 1},
             new Book{Id=2,Quantity = 0},
             new Book{Id=3,Quantity = 0},
             new Book{Id=4,Quantity = 0},
             new Book{Id=5,Quantity = 0},
             };
-            int price =100;
+            int price = 100;
             var target = new TDDHW2();
-            int actual =0;
+            int actual = 0;
 
-            int expected =100;
-            //act  
+            int expected = 100;
+            //act
             actual = target.Accounting(book, price);
             //Assert
-             Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
